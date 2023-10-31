b5bc89024b7a25703159a7801f288eeda500d9c0
Emmanuel Mwiti <kiuguemmanu@gmail.com>
2023-10-31 07:38:32 -0400

    -File refactor

--- AdditionTest.py
+++ AdditionTest.py
@@ -7,17 +7,5 @@ class TestAddition(unittest.TestCase):
         result = add_numbers(5, 3)
         self.assertEqual(result, 8, "Test case 1 (addition) failed")
 
-    def test_addition_negative_numbers(self):
-        result = add_numbers(-2, -7)
-        self.assertEqual(result, -9, "Test case 2 (addition) failed")
-
-    def test_addition_mixed_numbers(self):
-        result = add_numbers(10, -4)
-        self.assertEqual(result, 6, "Test case 3 (addition) failed")
-
-    def test_addition_float_numbers(self):
-        result = add_numbers(3.5, 1.5)
-        self.assertEqual(result, 5.0, "Test case 4 (addition) failed")
-
 if __name__ == "__main":
     unittest.main()
